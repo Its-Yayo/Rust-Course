fe1c494624a90bd1953f7ba8d6a1ca20ce455298
Its-Yayo <a01754574@itesm.mx>
2024-02-04 21:35:57 -0600

    unversioned files check

---
+++ Initial_Code/target/debug/Initial_Code.d
@@ -0,0 +1,1 @@
+D:\All\ Stuff\Programming\Rust\Rust-Course\Initial_Code\target\debug\Initial_Code.exe: D:\All\ Stuff\Programming\Rust\Rust-Course\Initial_Code\src\main.rs
---
+++ Initial_Code/target/debug/deps/Initial_Code.d
@@ -0,0 +1,5 @@
+D:\All Stuff\Programming\Rust\Rust-Course\Initial_Code\target\debug\deps\Initial_Code.exe: src\main.rs
+
+D:\All Stuff\Programming\Rust\Rust-Course\Initial_Code\target\debug\deps\Initial_Code.d: src\main.rs
+
+src\main.rs:
