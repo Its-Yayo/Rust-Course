35d0143ef20163de591efc0501d218b6e2ea31ca
Its-Yayo <a01754574@itesm.mx>
2024-02-04 21:43:36 -0600

    debug cargo lock fixed

--- Initial_Code/target/debug/Initial_Code.d
+++
@@ -1,1 +0,0 @@
-D:\All\ Stuff\Programming\Rust\Rust-Course\Initial_Code\target\debug\Initial_Code.exe: D:\All\ Stuff\Programming\Rust\Rust-Course\Initial_Code\src\main.rs
--- Initial_Code/target/debug/deps/Initial_Code-2947c2ff10039e77.d
+++
@@ -1,5 +0,0 @@
-D:\All Stuff\Programming\Rust\Rust-Course\Initial_Code\target\debug\deps\Initial_Code-2947c2ff10039e77.exe: src\main.rs
-
-D:\All Stuff\Programming\Rust\Rust-Course\Initial_Code\target\debug\deps\Initial_Code-2947c2ff10039e77.d: src\main.rs
-
-src\main.rs:
