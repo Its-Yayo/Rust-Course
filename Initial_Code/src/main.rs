3669f5c186e4e76d985db74d3d5f26a32c91e262
Yayo <88682615+Its-Yayo@users.noreply.github.com>
2024-02-21 18:25:22 -0600

    Update main.rs

--- Initial_Code/src/main.rs
+++ Initial_Code/src/main.rs
@@ -2,11 +2,12 @@
 use std::io;
 
 fn main() {
-    println!("Inout number game!");
-    println!("Type a number: ");
+    println!("Inout number game!")
+    println!("Type a number: ")
+    
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess).expect("Error reading guess");
 
-    println!("Number typed: {}", guess);
+    println!("Number typed: {}", guess)
 }
